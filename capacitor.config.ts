7656249ed94d7ce34564bcabbbe41cedb3c0bc26
vasilevv2017-netizen <vasilevv2017@gmail.com>
2026-01-08 22:10:02 +0200

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.etm.v333',
+  appName: 'ETM Module v3.33',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
